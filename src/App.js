7d6b19a9766fb21c8fc6227a4c5581c6d27be916
Martin laptop <info@setisbew.nl>
2023-02-25 14:55:50 +0100

    rebuild app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
-
+//theme primereact
+import "primereact/resources/themes/tailwind-light/theme.css";
+//core
+import "primereact/resources/primereact.min.css";
+//icons
+import "primeicons/primeicons.css";
+// import "primeflex/primeflex.css";
 
 function App() {
-  return (
-    <div >
-      Hello World
-    </div>
-  );
+  return <div>Hello World</div>;
 }
 
 export default App;
